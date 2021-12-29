17e22b8317d199e59007b3a82b1b2711df79a5a7
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-12-29 17:10:23 +1100

    mern stack

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require('./models/User');
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  'mongodb+srv://xueqi:guan@cluster0.yeumu.mongodb.net/MernStackDatabase?retryWrites=true&w=majority',
+);
+
+app.get('/getUsers', (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post('/createUser', async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log('Server runs!');
+});
